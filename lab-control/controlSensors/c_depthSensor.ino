baaeaf2a2d16328ff0666da4cab61018d473bfd2
StephenH17044 <g00256450@gmit.ie>
2021-02-21 23:21:44 +0000

    Second Commit (Control Sensors with Smoothing)

--- lab-control/controlSensors/c_depthSensor.ino
+++ lab-control/controlSensors/c_depthSensor.ino
@@ -1,48 +1,68 @@
 //++++++++++ Defining Pinouts ++++++++++
-
 #define PIN_SENSOR A1
 
 //++++++++++ Define PinMode ++++++++++
-
 #define pinMode(PIN_READ, INPUT);
 
-//++++++++++ Define Class ++++++++++
+//++++++++++ Define Array Varibles ++++++++++
+const int ArrElements = 20;               
+int Readings[ArrElements]; 
 
+//++++++++++ Define Distance Class ++++++++++
 class DistanceSensor {
   private:                                                                    // Defining Private Varibles
     unsigned long nextChangeTime = 0;
-    unsigned long OnTime; 
-    float SensorValue;                            
+    unsigned long OnTime;
+    int Value;                             
     float Voltage;
     float Distance;
     float DistancePoly;
+    int SensorAverage = 0;
+    int Total = 0;   
+    int ReadingIndex = 0;
     
-   
   public:                                                                     // Passing data into private varibles
-     DistanceSensor(float SensorValue, unsigned long OnTime) {
-        this->SensorValue = SensorValue;
+     DistanceSensor(int Value, unsigned long OnTime) {
+        this->Value = Value;
         this->OnTime = OnTime;
     }
     
-    void Check() {
+    void Check() {                                                           // Checks Value from sensor
       unsigned long currentTime = millis();
       
-      if(currentTime >= nextChangeTime){                                      // If time is greaterthan or equal to the values passed do this
-        SensorValue=analogRead(PIN_SENSOR);                                   // Read Analog Input
-        Voltage = SensorValue * (5.0 / 1023.0);                               // Convert to voltage
-        Distance = -21.75*(Voltage) + 58.021;                                 // Calculate Distance y=mx+c
-        DistancePoly = 12.275*pow(Voltage,4) - 92.759*pow(Voltage,3)          // Calculate Distance Polynomial
+      if(currentTime >= nextChangeTime){                                      
+        Voltage = SensorAverage * (5.0 / 1023.0);                             
+        Distance = -21.75*(Voltage) + 58.021;                                 
+        DistancePoly = 12.275*pow(Voltage,4) - 92.759*pow(Voltage,3)          
         + 252.03*pow(Voltage,2) - 303.81*(Voltage) + 156.35;
-        nextChangeTime = currentTime + OnTime;                                //Increase Time by passed value, equal intervals
-        Print();                                                              //Goto Routine Print
+        nextChangeTime = currentTime + OnTime;                                
+        Print();                                                              
       }
+    }
+
+    void Smooth() {                                                           //Smooths Analog Read Values             
+      Total = Total - Readings[ReadingIndex];     
+      Readings[ReadingIndex] = GetValue();     
+      Total = Total + Readings[ReadingIndex];
+      ReadingIndex = ReadingIndex + 1;
+      if (ReadingIndex >= ArrElements) {
+        ReadingIndex = 0;
+      }
+      SensorAverage = Total / ArrElements;
+      delay (1);      
     }
 
     void Print() {                                                            //Prints Data aquired from sensor at equal intervals
        Serial.print("Distance (y=mx+c): ");                               
-       Serial.println(Distance);
+       Serial.print(Distance);
+       Serial.print("     ");
        Serial.print("Distance Polynomial: ");  
        Serial.print(DistancePoly);                   
        Serial.println(" ");                                                
+    }
+
+    int GetValue(){                                                           // Read Analog Input 
+      int Value = analogRead(PIN_SENSOR);                                                  
+      return Value;
     }
 };
--- lab-control/controlSensors/z_main.ino
+++ lab-control/controlSensors/z_main.ino
@@ -2,16 +2,23 @@
 
 Blinker blink0 = Blinker(PIN_LED0, 1000, 500); 
 Blinker blink1 = Blinker(PIN_LED1, 1000, 500); 
-DistanceSensor DS = DistanceSensor(PIN_SENSOR,500);
+DistanceSensor DS = DistanceSensor(PIN_SENSOR,50);
+
+// ++++++++++ Program Setup ++++++++++
 
 void setup() { 
-Serial.begin(9600);
+  Serial.begin(9600);
+  //Initialize Values in Array
+  for (int i = 0; i < ArrElements; i++) {
+    Readings[i] = 0;
+  }      
 }
 
-// ++++++++++ Main Program ++++++++++1`11`1``11`
+// ++++++++++ Main Program ++++++++++
 
 void loop() {
   blink0.check();
   blink1.check();
+  DS.Smooth();
   DS.Check();
 }
